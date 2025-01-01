88c9415f39db1d82afe89e705250ecc30e3b5e55
twkwon0417 <kwontony123@konkuk.ac.kr>
2025-01-02 02:13:53 +0900

    feat: friendship implementation

--- src/main/java/com/kuit/moamoa/domain/Friendship.java
+++ src/main/java/com/kuit/moamoa/domain/Friendship.java
@@ -1,2 +1,41 @@
-package com.kuit.moamoa.domain;public class Friendship {
+package com.kuit.moamoa.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "friendships")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Friendship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long toUserId;
+
+    @Column(nullable = false)
+    private Long fromUserId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
 }

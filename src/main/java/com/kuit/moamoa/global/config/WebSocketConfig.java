9c1d09bcbe4925f6cbb392a8fe58eb0281f908b2
twkwon0417 <91003152+twkwon0417@users.noreply.github.com>
2025-02-09 00:23:45 +0900

    Update WebSocketConfig.java

--- src/main/java/com/kuit/moamoa/global/config/WebSocketConfig.java
+++ src/main/java/com/kuit/moamoa/global/config/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // stomp 접속 주소 url => /ws-stomp
         registry.addEndpoint("/ws-stomp")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
                 .withSockJS(); // SocketJS 지원
     }
 }

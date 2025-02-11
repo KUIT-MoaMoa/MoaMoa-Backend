09b793d7c81b2d4b88b83ce31c07fd715763d66f
jyun27 <kimjyun27@gmail.com>
2025-02-11 17:20:52 +0900

    fix: 반환 타입 변경

--- src/main/java/com/kuit/moamoa/controller/JoinController.java
+++ src/main/java/com/kuit/moamoa/controller/JoinController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "사용자 로그인", description = "사용자 회원가입 및 로그인")
@@ -20,16 +19,18 @@ public class JoinController {
 
     private final JoinService joinService;
 
-    @Operation(summary = "유저 일반 로그인", description = "서비스 내 간편 로그인 경로입니다.")
+    @Operation(summary = "유저 일반 로그인", description = "서비스 내 간편 로그인 경로입니다. Authorization 헤더의 JWT토큰이 유효해야 작동합니다.")
     @PostMapping("/login")
-    public String login(@ModelAttribute UserAuthRequest request) {
-        return "ok";
+    public ApiResponse<String> login(@ModelAttribute UserAuthRequest request) {
+        return new ApiResponse<>("로그인이 완료되었습니다.");
     }
+
 
     @Operation(summary = "유저 일반 회원가입", description = "서비스 내 간편 회원가입: 닉네임 설정 전, 가입 완료 경로입니다.")
     @PostMapping("/join")
-    public ResponseEntity<UserAuthResponse> join(@ModelAttribute UserAuthRequest request){
-        return ResponseEntity.ok(joinService.joinProcess(request));
+    public ApiResponse<UserAuthResponse> join(@ModelAttribute UserAuthRequest request){
+        UserAuthResponse userAuthResponse = joinService.joinProcess(request);
+        return new ApiResponse<>(userAuthResponse);
 
     }
 
@@ -41,11 +42,11 @@ public class JoinController {
         return new ApiResponse<>("비밀번호가 성공적으로 변경되었습니다.");
     }
 
-    @Operation(summary = "닉네임 설정", description = "소셜 로그인으로 회원가입 한 유저들이 닉네임을 설정하는 경로입니다.")
+    @Operation(summary = "닉네임 설정", description = "일반/소셜 회원가입 한 유저들이 닉네임을 설정하는 경로입니다.")
     @PostMapping("/nickname")
-    public String setNickname(@RequestBody NicknameRequest request){
+    public ApiResponse<String> setNickname(@RequestBody NicknameRequest request){
         joinService.setNickname(request);
-        return "ok";
+        return new ApiResponse<>("닉네임이 성공적으로 설정되었습니다.");
     }
 
 }

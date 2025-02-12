7c7c69f03c514c94b85c80ba234454dbd910d7b5
jyun27 <kimjyun27@gmail.com>
2025-02-13 02:58:24 +0900

    fix: 태그명 수정

--- src/main/java/com/kuit/moamoa/controller/OverspendingDiagnosisController.java
+++ src/main/java/com/kuit/moamoa/controller/OverspendingDiagnosisController.java
@@ -4,6 +4,7 @@ import com.kuit.moamoa.dto.request.OverspendingDiagnosisRequest;
 import com.kuit.moamoa.global.response.ApiResponse;
 import com.kuit.moamoa.service.OverspendingDiagnosisService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +13,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 @RequestMapping("/overspending-diagnosis")
+@Tag(name = "과소비 진단 프로세스", description = "")
 public class OverspendingDiagnosisController {
 
     private final OverspendingDiagnosisService overspendingDiagnosisService;
 
-    @Operation(summary = "과소비 진단", description = "과소비 진단 프로세스 경로입니다. 진단 결과가 저장되지 않습니다.")
+    @Operation(summary = "과소비 진단", description = "ageGroup변수 0:10~20대, 1:30대, 2:40대, 3:50대 이상으로 매핑돼있습니다.")
     @GetMapping("")
     public ApiResponse<String> diagnoseOverspending(@RequestBody OverspendingDiagnosisRequest request){
 

280a401dbeeb10a7c19b49bdd218b60d524114be
twkwon0417 <91003152+twkwon0417@users.noreply.github.com>
2025-02-16 19:42:20 +0900

    !hotfix: add timezone import

--- src/main/java/com/kuit/moamoa/MoamoaApplication.java
+++ src/main/java/com/kuit/moamoa/MoamoaApplication.java
@@ -1,5 +1,6 @@
 package com.kuit.moamoa;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;

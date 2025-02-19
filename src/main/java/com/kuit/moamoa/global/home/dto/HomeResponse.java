12ce1585cdf7c48cdc007290e0e5d0ec6881beff
rbqks529 <rbqks529@naver.com>
2025-02-19 12:57:04 +0900

    feat: HomeResponse 수정

--- src/main/java/com/kuit/moamoa/global/home/dto/HomeResponse.java
+++ src/main/java/com/kuit/moamoa/global/home/dto/HomeResponse.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class HomeResponse {
+    private String nickName;
     private boolean needOverConsumptionTest;
     private ConsumptionChallengeSummary consumptionChallengeSummary;
     private CoinSummary coinSummary;
     private ChallengeHomeResponse.ChallengeHomeSummaryResponse challengeHomeResponse;
 
-    public HomeResponse(boolean needOverConsumptionTest, ConsumptionChallengeSummary consumptionChallengeSummary, int coin, ChallengeHomeResponse.ChallengeHomeSummaryResponse challengeHomeResponse) { // ✅ 변경됨
+    public HomeResponse(String nickName, boolean needOverConsumptionTest, ConsumptionChallengeSummary consumptionChallengeSummary, int coin, ChallengeHomeResponse.ChallengeHomeSummaryResponse challengeHomeResponse) { // ✅ 변경됨
+        this.nickName = nickName;
         this.needOverConsumptionTest = needOverConsumptionTest;
         this.consumptionChallengeSummary = consumptionChallengeSummary;
         this.coinSummary = new CoinSummary(coin);
